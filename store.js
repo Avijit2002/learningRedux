741a725c5dc406091a244f138ef75e749444f49a
Avijit2002 <avijitram2013@gmail.com>
2024-02-04 16:52:55 +0530

    Action created function added

--- store.js
+++ store.js
@@ -1,24 +1,36 @@
 // Learing Redux in isolation
 
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialAccountState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialCustomerState = {
+  fullname: "",
+  id: "",
+  createdAt:""
+};
+
+
+function accountreducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposit": {
-      console.log(action)
+      console.log(action);
       return { ...state, balance: state.balance + action.payload };
     }
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan": {
-      if (state.balance > 0)
-        return { ...state, loan: state.loan + action.payload };
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        balance: state.balance + action.payload.loan,
+        loan: action.payload.loan,
+        loanPurpose: action.payload.loanPurpose,
+      };
     }
     case "account/payLoan":
       return { ...state, loan: 0 };
@@ -27,10 +39,49 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
-store.dispatch({
-  type: "account/deposit",
-  payload: 400,
-});
+
+function customerReducer(state = initialCustomerState, action){
+  switch(action.type){
+    case "customer/createCustomer":{
+      return {
+        ...state,
+        fullname: action.payload.fullname,
+        id: action.payload.id,
+        createdAt: action.payload.createdAt
+      }
+
+    }
+    default: return state
+  }
+}
+
+//Action creator function
+function deposit(amount) {
+  return {
+    type: "account/deposit",
+    payload: amount,
+  };
+}
+function createCustomer(name){
+  return {
+    type: "customer/createCustomer",
+    payload:{
+      fullname:name,
+      id: 2,
+      createdAt:new Date
+    }
+  }
+}
+
+//Creating store
+const rootReducer = combineReducers({
+  account : accountreducer,
+  customer : customerReducer
+})
+
+const store = createStore(rootReducer);
+store.dispatch(deposit(400));
+
+store.dispatch(createCustomer("avijit"))
 
 console.log(store.getState());
