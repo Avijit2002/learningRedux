9b78203ed31007f5ea9c761ac0d86c3ce506e6d8
Avijit2002 <avijitram2013@gmail.com>
2024-02-04 14:25:08 +0530

    redux-store added

--- store.js
+++ store.js
@@ -1,5 +1,7 @@
 // Learing Redux in isolation
 
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -8,16 +10,27 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "account/deposit":
+    case "account/deposit": {
+      console.log(action)
       return { ...state, balance: state.balance + action.payload };
+    }
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":{
-        if(state.balance>0)
+    case "account/requestLoan": {
+      if (state.balance > 0)
         return { ...state, loan: state.loan + action.payload };
     }
     case "account/payLoan":
       return { ...state, loan: 0 };
-    default : return state;
+    default:
+      return state;
   }
 }
+
+const store = createStore(reducer);
+store.dispatch({
+  type: "account/deposit",
+  payload: 400,
+});
+
+console.log(store.getState());
